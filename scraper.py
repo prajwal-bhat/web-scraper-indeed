4c139080be87a3269bb47cd1d4b59a7996d9fefd
Prajwal <prajwalbhatmr@gmail.com>
2019-03-13 12:35:46 +0530

    working scraper with multithreading, all functionalities are working

--- scraper.py
+++ scraper.py
@@ -1,29 +1,52 @@
 from urllib.request import urlopen
+from selenium import webdriver
 from bs4 import BeautifulSoup as soup
-import csv
+import json
+from multiprocessing.dummy import Pool  
+from multiprocessing import cpu_count
 
-base_url = "https://www.indeed.co.in/jobs?q=software+developer&l=Bengaluru,+Karnataka&start="
 
-for i in range(0, 1000, 10):
-    url = base_url+str(i)
+def scrap_to_json(url):    
     driver=webdriver.Chrome()
     driver.get(url)
     f = driver.page_source
     page_soup = soup(f, 'html.parser')
     driver.close()
     containers = page_soup.find_all('div', {'class':'jobsearch-SerpJobCard row result clickcard'})
+    jobs = []
     for container in containers:
         job_container = container.find('a',{'class':'turnstileLink'})
         job_title = job_container.get('title')
         job_link = "https://www.indeed.co.in"
         link = job_container.get('href')
         job_link = job_link + link
+        company_container = container.find('span',{'class':'company'})
+        company = company_container.get_text().strip()
+        # Exception handling if the job doesn't contain the salary details
         try:
             salary_container = container.find('span',{'class':'salary no-wrap'})
-            salary = salary_container.get_text().strip()
+            salary = salary_container.get_text().strip().replace('\u20b9','').replace(',','')
         except:
-            salary:"None"
-        print(job_title)
-        print(job_link)
-        print(salary)
-    
+            salary = "None"
+        # Create a dictionary for all the required values
+        job_dict = {
+            "Job Title":job_title,
+            "Company":company,
+            "Job Url":job_link,
+            "Salary":salary
+        }  
+        jobs.append(job_dict)
+    return jobs
+
+if __name__ == "__main__":
+    base_url = "https://www.indeed.co.in/jobs?q=software+developer&l=Bengaluru,+Karnataka&start="
+    url_list = []
+    for page in range(0,100,10):
+        url_list.append(base_url+str(page))
+    # Creates a Pool with cpu_count number of threads  
+    pool = Pool(cpu_count())  
+    results = pool.map(scrap_to_json, url_list)
+    pool.close()
+    # Dump all the values to json file 
+    with open('jobs.json', 'w') as f:
+        f.write(json.dumps(results, indent=4))
